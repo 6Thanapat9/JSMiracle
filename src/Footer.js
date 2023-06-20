28226439a8332d3544baa5548e85b526f0c39137
6Thanapat9 <135731506+6Thanapat9@users.noreply.github.com>
2023-06-21 01:38:52 +0700

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function Footer() {
+  return (
+    <div className='MyFooter'>
+      <span>Copyright 2023 @ Thanapat Piyawong</span>
+
+    </div>
+  );
+}
+
+export default Footer;
